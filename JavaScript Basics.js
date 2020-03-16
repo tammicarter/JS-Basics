32ce7f40f735eb125ae3e078d4dd532709e7e8b6
Tammi <tammicarter220@gmail.com>
2020-03-16 17:50:53 -0400

    Refactoring

--- JavaScript Basics.js
+++ JavaScript Basics.js
@@ -2,12 +2,16 @@
 /*
 Using a basic JS to practice HTML element controls
 create a Form that request user input via questions
-store the answer and create an output/ summary of user input
-*/
-let name = "Tammi Ross";
-alert(`Hi, ${name}. Want to see something cool?`);
+// store the answer and create an output/ summary of user input */ 
+    // Placing the <script src tag before the end of the </head> tag runs the JS file 1st
+    // Placing the same tag before the ending </body> tag runs HTML 1st -->
+
+//Testing connection
+let name= "Tammi Ross "; 
+alert(`Hi there, ${name} here. Want to see something cool?`);
+prompt(` Type Yes`);
 document.querySelector('html').innerHTML = `
-  <h1>Welcome to the ${name} Experience!</h1>
+  <h1>Welcome to the ${name} Experiment! Scroll Down!</h1>
   <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif">
 `;
  
